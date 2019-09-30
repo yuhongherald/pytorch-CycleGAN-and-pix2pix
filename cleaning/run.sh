fb54edeae09485c7cad8f1087a56f966c3a151bd
YuHongHerald <yuhongherald@gmail.com>
2019-09-30 18:17:35 +0800

    new stuff

--- cleaning/run.sh
+++ cleaning/run.sh
@@ -6,7 +6,10 @@ cd ..
 
 python datasets/combine_A_and_B.py --fold_A datasets/Macaws/contoured_cut --fold_B datasets/Macaws/contoured_outlines --fold_AB datasets/Macaws/contoured-cut-outlines
 
-python train.py --dataroot ./datasets/birds --name edge2birds --model pix2pix --direction BtoA --preprocess resize --load_size 64 --crop_size 64 --no_flip --netG unet_64 --netD n_layers --n_layers_D 2 --save_latest_freq 1334 --save_epoch_freq 20 --display_id 0 --niter 200 --niter_decay 0 --batch_size 4
+python train.py --dataroot ./datasets/birds --name edge2birds --model pix2pix --direction BtoA --preprocess resize --load_size 64 --crop_size 64 --no_flip --netG unet_64 --netD n_layers --n_layers_D 2 --save_latest_freq 1163 --save_epoch_freq 20 --display_id 0 --niter 200 --niter_decay 0 --batch_size 4
+
+# Epoch 179 - Rmb to add
+python train.py --dataroot ./datasets/birds --name edge2birds --model pix2pix --direction BtoA --preprocess resize --load_size 64 --crop_size 64 --no_flip --netG unet_64 --netD n_layers --n_layers_D 2 --save_latest_freq 1334 --save_epoch_freq 20 --display_id 0 --niter 200 --niter_decay 0 --batch_size 4 --continue_train --epoch_count 179
 
 # Epoch 79
 python train.py --dataroot ./datasets/birds --name edge2birds --model pix2pix --direction BtoA --preprocess resize --load_size 64 --crop_size 64 --no_flip --netG unet_64 --netD n_layers --n_layers_D 2 --save_latest_freq 1654 --save_epoch_freq 20 --display_id 0 --niter 200 --niter_decay 0 --batch_size 16 --continue_train --epoch_count 79
